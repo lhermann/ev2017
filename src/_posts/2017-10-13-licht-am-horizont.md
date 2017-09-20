9f1cc1ce1499638c04ad8fe6c92d92f7c47a274e
Lukas Hermann <lukher6@web.de>
2017-09-20 17:37:07 +0200

    Finished website content

---
+++ src/_posts/2017-10-13-licht-am-horizont.md
@@ -0,0 +1,10 @@
+---
+layout: post
+title:  "Thema 1: Licht am Horizont"
+date:   2017-10-13 07:23:55 +0100
+categories: video
+---
+
+<div class="o-ratio o-ratio--16:9">
+    <iframe src="https://www.youtube-nocookie.com/embed/IzmyvyRxoPo?rel=0&amp;showinfo=0" frameborder="0" allowfullscreen></iframe>
+</div>
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,20 @@
-const path = require('path');
+var webpack = require("webpack");
+var path = require("path");
+var PROD = process.env.NODE_ENV === "production";
 
 module.exports = {
-    entry: './src/_webpack/main.js',
+    entry: "./src/_webpack/main.js",
     output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'src/assets/js/')
+        filename: "bundle.js",
+        path: path.resolve(__dirname, "src/assets/js/")
     },
-    devtool: 'source-map',
-    target: 'node',
+    devtool: "source-map",
+    target: "node",
+    plugins: PROD
+        ? [
+              new webpack.optimize.UglifyJsPlugin({
+                  compress: { warnings: false }
+              })
+          ]
+        : []
 };
